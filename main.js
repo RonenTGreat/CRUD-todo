d72f0ae07299170e9dc92ac30d1ddce99ef831af
ronentgreat <ronenh53@gmail.com>
2022-06-07 22:36:39 +0000

    added edit and delete functionality

--- main.js
+++ main.js
@@ -71,3 +71,22 @@ let resetForm = () => {
   textarea.value = "";
 };
 
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+
+  data.splice(e.parentElement.parentElement.id, 1);
+
+  localStorage.setItem("data", JSON.stringify(data));
+
+  console.log(data);
+}
+
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+}
